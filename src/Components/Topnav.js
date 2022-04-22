23691512eacdbf59898e0a1ec70a035619a05d3e
Nawang17 <nawangofficial1010@gmail.com>
2022-04-22 13:15:50 -0400

    added home page

--- src/Components/Topnav.js
+++ src/Components/Topnav.js
@@ -3,12 +3,12 @@ import "../styles/Topnav.css";
 const Topnav = () => {
   return (
     <div className="topnav">
-      <div className="logo">Momos</div>
+      <div className="logo">momos</div>
       <div className="topnavright">
         <img
           loading="lazy"
           className="profileimg"
-          src="https://ui-avatars.com/api/?background=008AB8&color=fff&name=g&size=128"
+          src="https://res.cloudinary.com/dwzjfylgh/image/upload/v1648215217/dd23namcxikmc35qewa2.jpg"
           alt=""
         />
       </div>
